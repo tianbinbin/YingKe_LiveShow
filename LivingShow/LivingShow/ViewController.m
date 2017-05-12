0b3a3dc81f3fe0ea0c9539102a1648bc7c782110
田彬彬 <tbb@tianbinbindeMac-mini.local>
2017-05-12 14:13:21 +0800

    创建工程

---
+++ LivingShow/LivingShow/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LivingShow
+//
+//  Created by 田彬彬 on 2017/5/12.
+//  Copyright © 2017年 田彬彬. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ LivingShow/LivingShow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LivingShow
+//
+//  Created by 田彬彬 on 2017/5/12.
+//  Copyright © 2017年 田彬彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
